b6b2fd923a4fd0ae634baf21a7abbd70e1b5bff9
Siddartha Reddy Boreddy <144552624+SiddarthaBoreddy@users.noreply.github.com>
2025-06-24 15:49:22 -0700

    Add files via upload

---
+++ DTOs/TaskItemDto.cs
@@ -0,0 +1,9 @@
+namespace TaskManagerAPI.DTOs
+{
+    public class TaskItemDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
